26a23590e149740d5f9c3141f5a81ea1aec02cfb
nuki1048 <112338685+nuki1048@users.noreply.github.com>
2023-03-11 15:56:44 +0200

    Rework routing, now sintax is 6.4+

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+	BrowserRouter,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+	Routes,
+} from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 
 import { MainPage, ComicsPage, SingleComicPage, Page404 } from "../pages/Index";
 
 const App = () => {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<AppHeader />}>
+				<Route index element={<MainPage />} />
+				<Route element={<ComicsPage />} path="/comics" />
+				<Route element={<SingleComicPage />} path="/comics/:comicId" />
+				<Route element={<Page404 />} path="*" />
+			</Route>
+		)
+	);
 	return (
-		<BrowserRouter>
-			<div className="app">
-				<AppHeader />
-				<main>
-					<Routes>
-						<Route element={<MainPage />} path="/" />
-						<Route element={<ComicsPage />} path="/comics" />
-						<Route element={<SingleComicPage />} path="/comics/:comicId" />
-						<Route element={<Page404 />} path="*" />
-					</Routes>
-				</main>
-			</div>
-		</BrowserRouter>
+		<div className="app">
+			<main>
+				<RouterProvider router={router} />
+			</main>
+		</div>
 	);
 };
 
--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import React from "react";
 import "./AppHeader.scss";
 const AppHeader = () => {
@@ -6,29 +6,34 @@ const AppHeader = () => {
 		color: "#9f0013",
 	};
 	return (
-		<header className="app__header">
-			<h1 className="app__title">
-				<Link to="/">
-					<span>Marvel</span>
-					information portal
-				</Link>
-			</h1>
-			<nav className="app__menu">
-				<ul>
-					<li>
-						<NavLink end style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/">
-							Characters
-						</NavLink>
-					</li>
-					/
-					<li>
-						<NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/comics">
-							Comics
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<>
+			<header className="app__header">
+				<h1 className="app__title">
+					<Link to="/">
+						<span>Marvel</span>
+						information portal
+					</Link>
+				</h1>
+				<nav className="app__menu">
+					<ul>
+						<li>
+							<NavLink end style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/">
+								Characters
+							</NavLink>
+						</li>
+						/
+						<li>
+							<NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/comics">
+								Comics
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<div>
+				<Outlet />
+			</div>
+		</>
 	);
 };
 
